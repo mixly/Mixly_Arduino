d54c9617c1cc717eeaa8264efd9dae707b8dea52
luokailiang <bnu_l@126.com>
2016-03-17 14:23:01 +0800

    add board "Arduino/Genuino 101"

--- mixly_arduino/blockly/arduino.js
+++ mixly_arduino/blockly/arduino.js
@@ -152,6 +152,15 @@ var profile = {
 	interrupt : [["2", "2"], ["3", "3"]],
 	serial_select:[["Serial", "Serial"]],
     serial: 9600
+  },
+  'Arduino/Genuino 101': {
+    description: "",
+    digital: [["0", "0"],["1", "1"], ["2", "2"], ["3", "3"], ["4", "4"], ["5", "5"], ["6", "6"], ["7", "7"], ["8", "8"], ["9", "9"], ["10", "10"], ["11", "11"], ["12", "12"], ["13", "13"], ["A0", "A0"], ["A1", "A1"], ["A2", "A2"], ["A3", "A3"], ["A4", "A4"], ["A5", "A5"]],
+    analog: [["A0", "A0"], ["A1", "A1"], ["A2", "A2"], ["A3", "A3"], ["A4", "A4"], ["A5", "A5"]],
+	pwm:[["3", "3"], ["5", "5"], ["6", "6"], ["9", "9"]],
+	interrupt : [["2", "2"], ["5", "5"],["7", "7"], ["8", "8"],["10", "10"], ["11", "11"],["12", "12"], ["13", "13"]],
+	serial_select:[["Serial", "Serial"]],
+    serial: 9600
   }
 };
 profile["Arduino Yun"] =profile["Arduino Yun Mini"] =profile["Arduino Leonardo"] =profile["Arduino Leonardo ETH"] =profile["Arduino Micro"] =profile["Arduino Esplora"] =profile["LilyPad Arduino USB"] = profile["arduino_leonardo"];
