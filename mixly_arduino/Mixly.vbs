0de734c321c0c606cfc4d5819edad375a3053191
radiumray <radiumray9@gmail.com>
2016-04-08 16:13:01 +0800

    fix RGB bugs

--- mixly_arduino/Mixly.vbs
+++
@@ -1,2 +0,0 @@
-Set objShell = Wscript.CreateObject("Wscript.Shell")
-objShell.Run ".\arduino-1.7.8\java\bin\javaw -jar Mixly.jar " 
--- mixly_arduino/blockly/core/blockly.js
+++ mixly_arduino/blockly/core/blockly.js
@@ -98,8 +98,10 @@ Blockly.SPRITE = {
  * @param {number} hue Hue on a colour wheel (0-360).
  * @return {string} RGB code, e.g. '#5ba65b'.
  */
-
 Blockly.makeColour = function(hue) {
+  if(hue.toString().substring(0,1)=="#") {
+    return hue;
+  }
   return goog.color.hsvToHex(hue, Blockly.HSV_SATURATION,
       Blockly.HSV_VALUE * 255);
 };
