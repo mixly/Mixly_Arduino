89bb09bd1516a33e90ce8fc5b89756d2dcf2da3e
macomzzl <macom_zzl@163.com>
2018-07-11 10:44:15 +0800

    update display.js

--- mixly_arduino/blockly/converters/microbit_python/display.js
+++ mixly_arduino/blockly/converters/microbit_python/display.js
@@ -10,6 +10,63 @@ for (var i = 0; i < profile.default.builtinimg.length; i++) {
     }
 }
 
+
+function show_or_scroll(blockid1, blockid2) {
+    function converter(py2block, func, args, keywords, starargs, kwargs, node) {
+        if (args.length === 1 && keywords.length === 4) {//Image.ALL_CLOCKS, delay=120, wait=True, loop=False, clear=False
+            var imagesblock = null;
+            var delayblock = null;
+            var waitblock = null;
+            var loopblock = null;
+            var clearblock = null;
+
+            imagesblock = py2block.convert(args[0]);
+
+            for (var i = 0; i < keywords.length; i++) {
+                var param = keywords[i];
+                var key = py2block.identifier(param.arg);
+                if (key === "delay") {
+                    delayblock = py2block.convert(param.value);
+                } else if (key === "wait") {
+                    waitblock = py2block.identifier(param.value.id);
+                } else if (key === "loop") {
+                    loopblock = py2block.identifier(param.value.id);
+                } else if (key === "clear") {
+                    clearblock = py2block.identifier(param.value.id);
+                }
+            }
+
+            if (imagesblock != null && delayblock != null && waitblock != null && loopblock != null && clearblock != null) {
+                return [block(blockid2, func.lineno, {
+                        'wait': waitblock,
+                        'loop': loopblock,
+                        'clear': clearblock,
+                    },
+                    {
+                        'images': imagesblock,
+                        'delay': delayblock,
+                    }, {
+                        "inline": "true"
+                    })];
+            }
+        } else if (args.length === 1 && keywords.length === 0) {
+            return [block(blockid1, func.lineno, {},
+                {
+                    'PIN': py2block.convert(args[0]),
+                }, {
+                    "inline": "true"
+                })];
+        } else {
+            throw new Error("Incorrect number of arguments");
+        }
+    }
+    return converter;
+}
+
+pbc.moduleFunctionD.get('display')['show'] = show_or_scroll('monitor_show_image_or_string', 'microbit_display_show_animation');
+pbc.moduleFunctionD.get('display')['scroll'] = show_or_scroll('monitor_scroll_string', 'microbit_display_scroll_string_animation');
+
+
 pbc.moduleFunctionD.get('display')['on'] = function (py2block, func, args, keywords, starargs, kwargs, node) {
     if (args.length !== 0) {
         throw new Error("Incorrect number of arguments");
@@ -53,50 +110,6 @@ pbc.moduleFunctionD.get('display')['clear'] = function (py2block, func, args, ke
     })];
 }
 
-
-function show_or_scroll(mode) {
-    function converter(py2block, func, args, keywords, starargs, kwargs, node) {
-        if (args.length !== 1 || keywords.length !== 4) { //(baudrate=1000000, bits=8, mode=0, mosi=15, miso= 14, sclk=13)
-            throw new Error("Incorrect number of arguments");
-        }
-        var imageblock = py2block.convert(args[0]);
-        var delayblock = null;
-        var waitblock = null;
-        var loopblock = null;
-        var clearblock = null;
-        for (var i = 0; i < keywords.length; i++) {
-            var param = keywords[i];
-            var key = py2block.identifier(param.arg);
-            if (key === "delay") {
-                delayblock = py2block.convert(param.value);
-            } else if (key === "wait") {
-                waitblock = py2block.identifier(param.value.id);
-            } else if (key === "loop") {
-                loopblock = py2block.identifier(param.value.id);
-            } else if (key === "clear") {
-                clearblock = py2block.identifier(param.value.id);
-            }
-        }
-        if (imageblock != null && delayblock != null && waitblock != null && loopblock != null && clearblock != null) {
-            return [block("microbit_display_show_string", func.lineno, {
-                "MODE": mode,
-                "wait": waitblock,
-                "loop": loopblock,
-                "clear": clearblock
-            }, {
-                "images": imageblock,
-                "delay": delayblock
-            }, {
-                "inline": "true"
-            })];
-        }
-    }
-
-    return converter;
-}
-
-pbc.moduleFunctionD.get('display')['show'] = show_or_scroll('show');
-pbc.moduleFunctionD.get('display')['scroll'] = show_or_scroll('scroll');
 
 pbc.moduleFunctionD.get('mylcd')['on'] = function (py2block, func, args, keywords, starargs, kwargs, node) {
     if (args.length !== 0) {
@@ -146,84 +159,6 @@ pbc.moduleFunctionD.get('mylcd')['backlight'] = function (py2block, func, args, 
     });
 }
 
-
-pbc.moduleFunctionD.get('display')['show'] = function (py2block, func, args, keywords, starargs, kwargs, node) {
-
-    if (args.length === 1 && keywords.length === 4) {//Image.ALL_CLOCKS, delay=120, wait=True, loop=False, clear=False
-        var imagesblock = null;
-        var delayblock = null;
-        var waitblock = null;
-        var loopblock = null;
-        var clearblock = null;
-
-        imagesblock = py2block.convert(args[0]);
-
-        for (var i = 0; i < keywords.length; i++) {
-            var param = keywords[i];
-            var key = py2block.identifier(param.arg);
-            if (key === "delay") {
-                delayblock = py2block.convert(param.value);
-            } else if (key === "wait") {
-                waitblock = py2block.identifier(param.value.id);
-            } else if (key === "loop") {
-                loopblock = py2block.identifier(param.value.id);
-            } else if (key === "clear") {
-                clearblock = py2block.identifier(param.value.id);
-            }
-        }
-
-        if (imagesblock != null && delayblock != null && waitblock != null && loopblock != null && clearblock != null) {
-            return [block("microbit_display_show_animation", func.lineno, {
-                    'wait': waitblock,
-                    'loop': loopblock,
-                    'clear': clearblock,
-                },
-                {
-                    'images': imagesblock,
-                    'delay': delayblock,
-                }, {
-                    "inline": "true"
-                })];
-        }
-    } else if (args.length === 1 && keywords.length === 0) {
-        var defined = py2block.identifier(args[0].s);
-        if (defined != null) {
-            return [block("monitor_show_string", func.lineno, {"MODE": 'show'},
-                {
-                    'data': py2block.convert(args[0]),
-
-                }, {
-                    "inline": "true"
-                })];
-        } else {
-            return [block('microbit_display_show_image', func.lineno, {},
-                {
-                    'PIN': py2block.convert(args[0]),
-
-                }, {
-                    "inline": "true"
-                })];
-        }
-    } else {
-        throw new Error("Incorrect number of arguments");
-    }
-}
-
-
-//滚动显示字符串
-pbc.moduleFunctionD.get('display')['scroll'] = function (py2block, func, args, keywords, starargs, kwargs, node) {
-    if (args.length === 0) {
-        throw new Error("Incorrect number of arguments");
-    }
-
-    return [block("monitor_show_string", func.lineno, {"MODE": 'scroll'},
-        {
-            'data': py2block.convert(args[0]),
-
-        }, {
-            "inline": "true"
-        })];
-}
 //高度方法
 pbc.moduleFunctionD.get('image')['height'] = function (py2block, func, args, keywords, starargs, kwargs, node) {
     if (args.length === 0) {
