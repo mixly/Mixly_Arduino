d532e6fef63600cd0f76e61d47005c1d6de20987
Justinmht <33833002+Justinmht@users.noreply.github.com>
2018-08-20 11:56:02 +0800

    correct a few bug of ESP32,uodate py2block

--- mixly_arduino/blockly/generators/python/lists.js
+++ mixly_arduino/blockly/generators/python/lists.js
@@ -442,7 +442,7 @@ Blockly.Python.lists_find = function(){
   var varName = Blockly.Python.valueToCode(this, 'VAR', Blockly.Python.ORDER_ASSIGNMENT) || '0';
   var argument = Blockly.Python.valueToCode(this, 'data', Blockly.Python.ORDER_ASSIGNMENT) || '0';
   if (op=='INDEX')
-    var code = varName + '.index('  + argument + ')';
+    var code = varName + '.find('  + argument + ')';
   else if (op=='COUNT')
     var code = varName + '.count('  + argument + ')';
   return [code, Blockly.Python.ORDER_ATOMIC];
