dc62918f944ae4415d99afa4f791b4d9a6a6a9a7
fredqian <fredqian@bnu.edu.cn>
2018-10-14 17:46:13 +0800

    update jar and index

--- mixly_arduino/esp32Build/lib/dhtx.py
+++ mixly_arduino/esp32Build/lib/dhtx.py
@@ -7,6 +7,7 @@ def get_temperature(sensetype,pin):
 	else:
 		d = dht.DHT22(machine.Pin(pin))
 	d.measure()
+	time.sleep_ms(1)
 	return d.temperature() # eg. 23 (°C)
 def get_humidity(sensetype,pin):
 	if (sensetype=='dht11'):
@@ -14,6 +15,7 @@ def get_humidity(sensetype,pin):
 	else:
 		d = dht.DHT22(machine.Pin(pin))
 	d.measure()
+	time.sleep_ms(1)
 	return d.humidity()
 def get_tempandhum(sensetype,pin):
 	if (sensetype=='dht11'):
@@ -21,4 +23,5 @@ def get_tempandhum(sensetype,pin):
 	else:
 		d = dht.DHT22(machine.Pin(pin))
 	d.measure()
+	time.sleep_ms(1)
 	return d.temperature(),d.humidity()
