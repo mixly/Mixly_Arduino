f92a43c719dc60954e96a9945972b6f61cd02138
Justinmht <33833002+Justinmht@users.noreply.github.com>
2018-10-18 14:53:23 +0800

    update monitor_bright_point

--- mixly_arduino/blockly/msg/js/zh-hans.js
+++ mixly_arduino/blockly/msg/js/zh-hans.js
@@ -1955,3 +1955,4 @@ Blockly.MIXLY_ESP32_RNUMBER = "读取数字 地址";
 Blockly.MIXLY_ESP32_READ = "读取";
 Blockly.MIXLY_ESP32_SUCCESS = "成功吗？";
 Blockly.MIXLY_ESP32_SPI_INIT = "SPI 初始化";
+Blockly.MIXLY_TOOLTIP_DISPLAY_ONOFF='返回灯的亮灭状态';
