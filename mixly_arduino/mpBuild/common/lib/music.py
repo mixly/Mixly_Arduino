b2230b22dd2b4906d98df397b4e46b96f70252f2
xiebochao <xbc1992@yeah.net>
2018-11-17 22:02:08 +0800

    fix some bug

--- mixly_arduino/mpBuild/common/lib/music.py
+++ mixly_arduino/mpBuild/common/lib/music.py
@@ -107,76 +107,67 @@ POWER_DOWN = ['g5:1', 'd#', 'c', 'g4:2', 'b:1', 'c5:3']
 
 normal_tone = {
     'A1': 55, 'B1': 62, 'C1': 33, 'D1': 37, 'E1': 41, 'F1': 44, 'G1': 49,
-
     'A2': 110, 'B2': 123, 'C2': 65, 'D2': 73, 'E2': 82, 'F2': 87, 'G2': 98,
-
     'A3': 220, 'B3': 247, 'C3': 131, 'D3': 147, 'E3': 165, 'F3': 175, 'G3': 196,
-
     'A4': 440, 'B4': 494, 'C4': 262, 'D4': 294, 'E4': 330, 'F4': 349, 'G4': 392,
-
     'A5': 880, 'B5': 988, 'C5': 523, 'D5': 587, 'E5': 659, 'F5': 698, 'G5': 784,
-
     'A6': 1760, 'B6': 1976, 'C6': 1047, 'D6': 1175, 'E6': 1319, 'F6': 1397, 'G6': 1568,
-
     'A7': 3520, 'B7': 3951, 'C7': 2093, 'D7': 2349, 'E7': 2637, 'F7': 2794, 'G7': 3135,
-
     'A8': 7040, 'B8': 7902, 'C8': 4186, 'D8': 4699, 'E8': 5274, 'F8': 5588, 'G8': 6271,
-
     'A9': 14080, 'B9': 15804
 }
 
 rising_tone = {
     'A1': 58, 'C1': 35, 'D1': 39, 'F1': 46, 'G1': 52,
-
     'A2': 117, 'C2': 69, 'D2': 78, 'F2': 93, 'G2': 104,
-
     'A3': 233, 'C3': 139, 'D3': 156, 'F3': 185, 'G3': 208,
-
     'A4': 466, 'C4': 277, 'D4': 311, 'F4': 370, 'G4': 415,
-
     'A5': 932, 'C5': 554, 'D5': 622, 'F5': 740, 'G5': 831,
-
     'A6': 1865, 'C6': 1109, 'D6': 1245, 'F6': 1480, 'G6': 1661,
-
     'A7': 3729, 'C7': 2217, 'D7': 2489, 'F7': 2960, 'G7': 3322,
-
     'A8': 7459, 'C8': 4435, 'D8': 4978, 'F8': 5920, 'G8': 6645,
-
     'A9': 14917
 }
 
 falling_tone = {
     'B1': 58, 'D1': 35, 'E1': 39, 'G1': 46, 'A1': 52,
-
     'B2': 117, 'D2': 69, 'E2': 78, 'G2': 93, 'A2': 104,
-
     'B3': 233, 'D3': 139, 'E3': 156, 'G3': 185, 'A3': 208,
-
     'B4': 466, 'D4': 277, 'E4': 311, 'G4': 370, 'A4': 415,
-
     'B5': 932, 'D5': 554, 'E5': 622, 'G5': 740, 'A5': 831,
-
     'B6': 1865, 'D6': 1109, 'E6': 1245, 'G6': 1480, 'A6': 1661,
-
     'B7': 3729, 'D7': 2217, 'E7': 2489, 'G7': 2960, 'A7': 3322,
-
     'B8': 7459, 'D8': 4435, 'E8': 4978, 'G8': 5920, 'A8': 6645,
-
     'B9': 14917
 }
 
 Letter = 'ABCDEFG#R'
+beat = 60000 / 4 / 120
+ticks = octave = duration = 4
+bpm = 120
 
 def set_tempo(ticks=4, bpm=120):
+    global ticks, bpm, beat
     ticks = ticks
     bpm = bpm
     beat = 60000 / bpm / ticks
 
+def get_tempo():
+    return (ticks,bpm)
+
 def set_octave(octave=4):
+    global octave
     octave = octave
 
+def get_octave():
+    return octave
+
 def set_duration(duration=4):
+    global duration
     duration = duration
+
+def get_duration():
+    return duration
 
 def reset():
     set_duration()
@@ -221,12 +212,11 @@ def set_default(tone):
         set_duration(int(tone[(pos + 1):]))
         tone = tone[:pos]
 
-def play(tune, pin=27, duration=None):
+def play(tune, pwm, duration=None):
     from machine import Pin, PWM
     from utime import sleep_ms
 
     try:
-        pwm = PWM(Pin(pin))
         if duration is None:
             set_default(tune[0])
         else:
@@ -242,30 +232,15 @@ def play(tune, pin=27, duration=None):
     finally:
         pwm.deinit()
 
-def pitch(freq, tim=512, pin=27):
+def pitch(freq, pwm):
     from machine import Pin, PWM
     from utime import sleep_ms
 
-    try:
-        pwm = PWM(Pin(pin))
-        pwm.freq(freq)  # set frequency
-        pwm.duty(tim)  # set duty cycle
-        sleep_ms(tim)
-    finally:
-        pwm.deinit()
-
-# def pitch(freq,pin):
-# 	if (freq>1):
-# 		pwm=PWM(Pin(pin))
-# 		pwm.duty(512)
-# 		pwm.freq(freq)
-# 	else:
-# 		pwm=PWM(Pin(pin))
-# 		pwm.duty(0)
-# 		pwm.freq(1)
-	
-def stop(pin=27):
-	pwm=PWM(Pin(pin))
-	pwm.duty(0)
-	pwm.freq(1)
-	pwm.deinit()
+    pwm.duty(512)  # set duty cycle
+    pwm.freq(freq)  # set frequency
+    
+def stop(pwm):
+    from machine import Pin, PWM
+    pwm.duty(0)
+    pwm.freq(1)
+    pwm.deinit()
