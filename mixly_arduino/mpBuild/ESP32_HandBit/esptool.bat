8e1e1387a5d2e3ed3488644240e7049c8b9acb92
fredqian <fredqian@bnu.edu.cn>
2018-12-14 15:12:09 +0800

    add handbit bin

--- mixly_arduino/mpBuild/ESP32_HandBit/esptool.bat
+++ mixly_arduino/mpBuild/ESP32_HandBit/esptool.bat
@@ -1,3 +1,3 @@
 @echo off
 cd mpBuild/common
-esptool.exe --port %2 --baud %1 erase_flash && esptool.exe --port %2 --baud %1 write_flash 0x1000 ../ESP32_MixGo/esp32.bin 
+esptool.exe --port %2 --baud %1 erase_flash && esptool.exe --port %2 --baud %1 write_flash -ff=40m -fm=dio -fs=8MB 0x0000 ../ESP32_HandBit/target.bin
--- mixly_arduino/mpBuild/mixly.py
+++
@@ -1,8 +0,0 @@
-import machine
-import mixgo
-import time
-
-
-while True:
-    print(mixgo.Infrared_left.near())
-    time.sleep_ms(100)
