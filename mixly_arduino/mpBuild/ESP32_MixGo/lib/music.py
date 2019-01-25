13f13b87b19d33e0f0b80f21920b72e3f520ef15
xiebochao <xbc1992@yeah.net>
2019-01-25 21:19:29 +0800

    Temporarily delete matrix_show in music.py

--- mixly_arduino/mpBuild/ESP32_MixGo/lib/music.py
+++ mixly_arduino/mpBuild/ESP32_MixGo/lib/music.py
@@ -5,30 +5,30 @@
 # Licensed under the MIT license:
 #   http://www.opensource.org/licenses/mit-license.php
 #
-import matrix
+#import matrix
 
-DADADADUM = ['r4:2', 'g', 'g', 'g', 'eb:8', 'r:2', 'f', 'f', 'f', 'd:8']
+DADADADUM = ('r4:2', 'g', 'g', 'g', 'eb:8', 'r:2', 'f', 'f', 'f', 'd:8')
 
-ENTERTAINER = [
+ENTERTAINER = (
     'd4:1', 'd#', 'e', 'c5:2', 'e4:1', 'c5:2', 'e4:1', 'c5:3', 'c:1', 'd',
     'd#', 'e', 'c', 'd', 'e:2', 'b4:1', 'd5:2', 'c:4'
-]
+)
 
-PRELUDE = [
+PRELUDE = (
     'c4:1', 'e', 'g', 'c5', 'e', 'g4', 'c5', 'e', 'c4', 'e', 'g', 'c5', 'e',
     'g4', 'c5', 'e', 'c4', 'd', 'g', 'd5', 'f', 'g4', 'd5', 'f', 'c4', 'd',
     'g', 'd5', 'f', 'g4', 'd5', 'f', 'b3', 'd4', 'g', 'd5', 'f', 'g4', 'd5',
     'f', 'b3', 'd4', 'g', 'd5', 'f', 'g4', 'd5', 'f', 'c4', 'e', 'g', 'c5',
     'e', 'g4', 'c5', 'e', 'c4', 'e', 'g', 'c5', 'e', 'g4', 'c5', 'e'
-]
+)
 
-ODE = [
+ODE = (
     'e4', 'e', 'f', 'g', 'g', 'f', 'e', 'd', 'c', 'c', 'd', 'e', 'e:6', 'd:2',
     'd:8', 'e:4', 'e', 'f', 'g', 'g', 'f', 'e', 'd', 'c', 'c', 'd', 'e', 'd:6',
     'c:2', 'c:8'
-]
+)
 
-NYAN = [
+NYAN = (
     'f#5:1', 'g#', 'c#:1', 'd#:2', 'b4:1', 'd5:1', 'c#', 'b4:2', 'b', 'c#5',
     'd', 'd:1', 'c#', 'b4:1', 'c#5:1', 'd#', 'f#', 'g#', 'd#', 'f#', 'c#', 'd',
     'b4', 'c#5', 'b4', 'd#5:2', 'f#', 'g#:1', 'd#', 'f#', 'c#', 'd#', 'b4',
@@ -39,72 +39,72 @@ NYAN = [
     'f#:1', 'g#', 'b:2', 'f#:1', 'g#', 'b', 'b', 'c#5', 'd#', 'b4', 'f#', 'g#',
     'f#', 'b:2', 'b:1', 'a#', 'b', 'f#', 'g#', 'b', 'e5', 'd#', 'e', 'f#',
     'b4:2', 'c#5'
-]
+)
 
-RINGTONE = [
+RINGTONE = (
     'c4:1', 'd', 'e:2', 'g', 'd:1', 'e', 'f:2', 'a', 'e:1', 'f', 'g:2', 'b',
     'c5:4'
-]
+)
 
-FUNK = [
+FUNK = (
     'c2:2', 'c', 'd#', 'c:1', 'f:2', 'c:1', 'f:2', 'f#', 'g', 'c', 'c', 'g',
     'c:1', 'f#:2', 'c:1', 'f#:2', 'f', 'd#'
-]
+)
 
-BLUES = [
+BLUES = (
     'c2:2', 'e', 'g', 'a', 'a#', 'a', 'g', 'e', 'c2:2', 'e', 'g', 'a', 'a#',
     'a', 'g', 'e', 'f', 'a', 'c3', 'd', 'd#', 'd', 'c', 'a2', 'c2:2', 'e', 'g',
     'a', 'a#', 'a', 'g', 'e', 'g', 'b', 'd3', 'f', 'f2', 'a', 'c3', 'd#',
     'c2:2', 'e', 'g', 'e', 'g', 'f', 'e', 'd'
-]
+)
 
-BIRTHDAY = [
+BIRTHDAY = (
     'c4:4', 'c:1', 'd:4', 'c:4', 'f', 'e:8', 'c:3', 'c:1', 'd:4', 'c:4', 'g',
     'f:8', 'c:3', 'c:1', 'c5:4', 'a4', 'f', 'e', 'd', 'a#:3', 'a#:1', 'a:4',
     'f', 'g', 'f:8'
-]
+)
 
-WEDDING = [
+WEDDING = (
     'c4:4', 'f:3', 'f:1', 'f:8', 'c:4', 'g:3', 'e:1', 'f:8', 'c:4', 'f:3',
     'a:1', 'c5:4', 'a4:3', 'f:1', 'f:4', 'e:3', 'f:1', 'g:8'
-]
+)
 
-FUNERAL = [
+FUNERAL = (
     'c3:4', 'c:3', 'c:1', 'c:4', 'd#:3', 'd:1', 'd:3', 'c:1', 'c:3', 'b2:1',
     'c3:4'
-]
+)
 
-PUNCHLINE = ['c4:3', 'g3:1', 'f#', 'g', 'g#:3', 'g', 'r', 'b', 'c4']
+PUNCHLINE = ('c4:3', 'g3:1', 'f#', 'g', 'g#:3', 'g', 'r', 'b', 'c4')
 
-PYTHON = [
+PYTHON = (
     'd5:1', 'b4', 'r', 'b', 'b', 'a#', 'b', 'g5', 'r', 'd', 'd', 'r', 'b4',
     'c5', 'r', 'c', 'c', 'r', 'd', 'e:5', 'c:1', 'a4', 'r', 'a', 'a', 'g#',
     'a', 'f#5', 'r', 'e', 'e', 'r', 'c', 'b4', 'r', 'b', 'b', 'r', 'c5', 'd:5',
     'd:1', 'b4', 'r', 'b', 'b', 'a#', 'b', 'b5', 'r', 'g', 'g', 'r', 'd', 'c#',
     'r', 'a', 'a', 'r', 'a', 'a:5', 'g:1', 'f#:2', 'a:1', 'a', 'g#', 'a',
     'e:2', 'a:1', 'a', 'g#', 'a', 'd', 'r', 'c#', 'd', 'r', 'c#', 'd:2', 'r:3'
-]
+)
 
-BADDY = ['c3:3', 'r', 'd:2', 'd#', 'r', 'c', 'r', 'f#:8']
+BADDY = ('c3:3', 'r', 'd:2', 'd#', 'r', 'c', 'r', 'f#:8')
 
-CHASE = [
+CHASE = (
     'a4:1', 'b', 'c5', 'b4', 'a:2', 'r', 'a:1', 'b', 'c5', 'b4', 'a:2', 'r',
     'a:2', 'e5', 'd#', 'e', 'f', 'e', 'd#', 'e', 'b4:1', 'c5', 'd', 'c',
     'b4:2', 'r', 'b:1', 'c5', 'd', 'c', 'b4:2', 'r', 'b:2', 'e5', 'd#', 'e',
     'f', 'e', 'd#', 'e'
-]
+)
 
-BA_DING = ['b5:1', 'e6:3']
+BA_DING = ('b5:1', 'e6:3')
 
-WAWAWAWAA = ['e3:3', 'r:1', 'd#:3', 'r:1', 'd:4', 'r:1', 'c#:8']
+WAWAWAWAA = ('e3:3', 'r:1', 'd#:3', 'r:1', 'd:4', 'r:1', 'c#:8')
 
-JUMP_UP = ['c5:1', 'd', 'e', 'f', 'g']
+JUMP_UP = ('c5:1', 'd', 'e', 'f', 'g')
 
-JUMP_DOWN = ['g5:1', 'f', 'e', 'd', 'c']
+JUMP_DOWN = ('g5:1', 'f', 'e', 'd', 'c')
 
-POWER_UP = ['g4:1', 'c5', 'e4', 'g5:2', 'e5:1', 'g5:3']
+POWER_UP = ('g4:1', 'c5', 'e4', 'g5:2', 'e5:1', 'g5:3')
 
-POWER_DOWN = ['g5:1', 'd#', 'c', 'g4:2', 'b:1', 'c5:3']
+POWER_DOWN = ('g5:1', 'd#', 'c', 'g4:2', 'b:1', 'c5:3')
 
 normal_tone = {
     'A1': 55, 'B1': 62, 'C1': 33, 'D1': 37, 'E1': 41, 'F1': 44, 'G1': 49,
@@ -227,7 +227,30 @@ class MIDI():
             sleep_ms(midi[1])
         pwm.deinit()
 
-    def play_show(self, tune, pin=27, display=matrix.display, duration=None):
+    # def play_show(self, tune, pin=27, display=matrix.display, duration=None):
+    #     from machine import Pin, PWM
+    #     from utime import sleep_ms
+    #     pwm = PWM(Pin(pin))
+    #     if duration is None:
+    #         self.set_default(tune[0])
+    #     else:
+    #         self.set_duration(duration)
+    #     for tone in tune:
+    #         tone = tone.upper()  # all to upper
+    #         if tone[0] not in Letter:
+    #             continue
+    #         if len(tone)==4:
+    #             display.showstatic(tone.replace(":",""))
+    #         else:
+    #             display.showstatic(tone)
+    #         midi = self.midi(tone)
+    #         pwm.freq(midi[0])  # set frequency
+    #         #pwm.duty(midi[1])  # set duty cycle
+    #         sleep_ms(midi[1])
+    #     display.clear()
+    #     pwm.deinit()
+
+    def play_show(self, tune, pin=27, duration=None):
         from machine import Pin, PWM
         from utime import sleep_ms
         pwm = PWM(Pin(pin))
@@ -239,15 +262,12 @@ class MIDI():
             tone = tone.upper()  # all to upper
             if tone[0] not in Letter:
                 continue
-            if len(tone)==4:
-                display.showstatic(tone.replace(":",""))
             else:
-                display.showstatic(tone)
+                print(tone)
             midi = self.midi(tone)
             pwm.freq(midi[0])  # set frequency
             #pwm.duty(midi[1])  # set duty cycle
             sleep_ms(midi[1])
-        display.clear()
         pwm.deinit()
 
     def pitch(self, pin=27, freq=440, tim=0):
