0d1d70a8de8a14e6aea468f43d47b98c29d493fb
macomzzl <macom_zzl@163.com>
2019-08-03 16:28:31 +0800

    change sha1 to sha256 for support aiot board

--- mixly_arduino/mpBuild/common/lib/mixly_tool.py
+++ mixly_arduino/mpBuild/common/lib/mixly_tool.py
@@ -1,18 +1,18 @@
 import hashlib
 import os
 
-def sha1_file(f):
+def sha_file(f):
     if f not in set(os.listdir(".")):
-        return 'None:::sha1_file_end'
+        return 'None:::sha_file_end'
     else:
-        sha = hashlib.sha1()
+        sha = hashlib.sha256()
         with open(f, encoding='utf-8') as fd:
             file_buffer = fd.read(128).encode("utf-8")
             while len(file_buffer) > 0:
                 sha.update(file_buffer)
                 file_buffer = fd.read(128).encode("utf-8")
             h = sha.digest()
-            return ''.join(['%.2x' % i for i in h]) + ":::sha1_file_end"
+            return ''.join(['%.2x' % i for i in h]) + ":::sha_file_end"
 
 def reload(mod):
     import sys
