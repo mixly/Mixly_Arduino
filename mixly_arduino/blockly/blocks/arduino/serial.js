e7bd3bc94b2df2d8dd2df2b2dda1bf156cafd6e5
user <qiujiongtao@163.com>
2019-09-17 23:18:13 +0800

    Update serial.js

--- mixly_arduino/blockly/blocks/arduino/serial.js
+++ mixly_arduino/blockly/blocks/arduino/serial.js
@@ -133,38 +133,39 @@ Blockly.Blocks['serial_parseInt_Float'] = {
        this.setTooltip(Blockly.MIXLY_TOOLTIP_SERIAL_FLUSH);
      }
    };
-
    Blockly.Blocks['serial_softserial'] = {
     init: function() {
-     this.setColour(Blockly.Blocks.serial.HUE);
-     this.appendValueInput("RX", Number)
-     .appendField(Blockly.MIXLY_SETUP)
-     .appendField(new Blockly.FieldDropdown(profile.softserial_select), "serial_select")
-     .appendField("RX#")
-     .setCheck(Number)
-     .setAlign(Blockly.ALIGN_RIGHT);
-     this.appendValueInput("TX", Number)
-     .appendField("TX#")
-     .setCheck(Number)
-     .setAlign(Blockly.ALIGN_RIGHT);
-     this.setPreviousStatement(true, null);
-     this.setNextStatement(true, null);
-     this.setInputsInline(true);
-     this.setTooltip(Blockly.MIXLY_TOOLTIP_SOFTSERIAL.replace('%1',Blockly.Arduino.valueToCode(this, 'RX',Blockly.Arduino.ORDER_ATOMIC))
-      .replace('%2',Blockly.Arduino.valueToCode(this, 'TX',Blockly.Arduino.ORDER_ATOMIC)));
-   }
- };
+      this.setColour(Blockly.Blocks.serial.HUE);
+      this.appendDummyInput("")
+      .appendField(Blockly.MIXLY_SETUP)
+      .appendField(new Blockly.FieldDropdown(profile.default.serial_select), "serial_select")
+      .appendField(Blockly.MIXLY_SERIAL_BEGIN);
+      this.appendValueInput("RX", Number)
+      .setCheck(Number)
+      .appendField("RX#")
+      .setAlign(Blockly.ALIGN_RIGHT);
+      this.appendValueInput("TX", Number)
+      .appendField("TX#")
+      .setCheck(Number)
+      .setAlign(Blockly.ALIGN_RIGHT);
+      this.setPreviousStatement(true, null);
+      this.setNextStatement(true, null);
+      this.setInputsInline(true);
+      this.setTooltip(Blockly.MIXLY_TOOLTIP_SOFTSERIAL.replace('%1',Blockly.Arduino.valueToCode(this, 'RX',Blockly.Arduino.ORDER_ATOMIC))
+        .replace('%2',Blockly.Arduino.valueToCode(this, 'TX',Blockly.Arduino.ORDER_ATOMIC)));
+    }
+  };
 
- Blockly.Blocks['serial_event'] = {
-  init: function () {
-    this.setColour(Blockly.Blocks.serial.HUE);
-    this.appendDummyInput()
-    .appendField(new Blockly.FieldDropdown(profile.default.serial_select), "serial_select")
-    .appendField(Blockly.MIXLY_SERIAL_EVENT);
-    this.appendStatementInput('DO')
-    .appendField(Blockly.MIXLY_DO);
-    this.setPreviousStatement(true);
-    this.setNextStatement(true);
-    this.setTooltip(Blockly.MIXLY_TOOLTIP_SERIALEVENT);
-  }
-};
+  Blockly.Blocks['serial_event'] = {
+    init: function () {
+      this.setColour(Blockly.Blocks.serial.HUE);
+      this.appendDummyInput()
+      .appendField(new Blockly.FieldDropdown(profile.default.serial_select), "serial_select")
+      .appendField(Blockly.MIXLY_SERIAL_EVENT);
+      this.appendStatementInput('DO')
+      .appendField(Blockly.MIXLY_DO);
+      this.setPreviousStatement(true);
+      this.setNextStatement(true);
+      this.setTooltip(Blockly.MIXLY_TOOLTIP_SERIALEVENT);
+    }
+  };
