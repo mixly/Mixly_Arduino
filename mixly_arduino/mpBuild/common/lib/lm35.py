6004d3894dc61885d309f1bfc538319f394e7c8c
fredqian <fredqian@bnu.edu.cn>
2019-12-27 10:33:52 +1000

    fix jar for import

--- mixly_arduino/mpBuild/common/lib/lm35.py
+++ mixly_arduino/mpBuild/common/lib/lm35.py
@@ -4,4 +4,4 @@ def get_LM35_temperature(val):
 	my_lm35 = machine.ADC(machine.Pin(val))
 	my_lm35.atten(machine.ADC.ATTN_11DB)
 
-    return (my_lm35.read() / 4096) * 5000 / 10.24
+	return (my_lm35.read() / 4096) * 5000 / 10.24
