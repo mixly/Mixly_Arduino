78f3e834745db94f000f16d6b852ab9709c3bc91
xiebochao <xbc1992@yeah.net>
2020-01-16 18:52:50 +0800

    Update reset.py

--- mixly_arduino/mpBuild/ESP32_MixGo/lib/reset.py
+++ mixly_arduino/mpBuild/ESP32_MixGo/lib/reset.py
@@ -2,11 +2,8 @@ import machine
 from machine import I2C
 from machine import Pin
 
-i2c = I2C(scl = Pin(22), sda = Pin(21), freq = 100000)
 buf = bytearray(1)
-pins = [0,2,4,5,18,21,22,19,23,20,15,12,13,27,14,25,26,17,16,32,33]
-for i in pins:
-    Pin(i, Pin.OUT).value(0)    
+i2c = I2C(scl = Pin(22), sda = Pin(21), freq = 100000)
 
 try:
     i2c.readfrom_mem_into(0x68, 0X75, buf)
@@ -14,11 +11,8 @@ except:
     pass
 else:
     if buf[0] == 0x71:
-        Pin(0, Pin.OUT).value(1)
-        Pin(5, Pin.OUT).value(1)
-    else:
-        Pin(0, Pin.OUT).value(0)
-        Pin(5, Pin.OUT).value(0)
+        for i in (0,5):
+            Pin(i, Pin.OUT).value(1)    
 
 try:
     from matrix import display
