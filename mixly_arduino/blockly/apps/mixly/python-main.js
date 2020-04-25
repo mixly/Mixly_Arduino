4f46eb72f7d1cabcfd13d368191460dbfd386bdd
xiebochao <xbc1992@yeah.net>
2020-04-25 11:22:21 +0800

    fix bug for micro:bit

--- mixly_arduino/blockly/apps/mixly/python-main.js
+++ mixly_arduino/blockly/apps/mixly/python-main.js
@@ -18,6 +18,13 @@ function getHexFile(firmware) {
 	}else{
 		code = Blockly.Python.workspaceToCode(Blockly.mainWorkspace) || '';
 	}
+	code = code_head+code
+	code = code.replace('from mixpy import math_map','')
+	code = code.replace('from mixpy import math_mean','')
+	code = code.replace('from mixpy import math_median','')
+	code = code.replace('from mixpy import math_modes','')
+	code = code.replace('from mixpy import math_standard_deviation','')
+	code = code.replace('from mixpy import lists_sort','')
 	var hexlified_python = hexlify(code);
 	var insertion_point = ":::::::::::::::::::::::::::::::::::::::::::";
 	return firmware.replace(insertion_point, hexlified_python);
@@ -68,3 +75,63 @@ function hexlify(script) {
 	}
 	return output.join('\n');
 };
+
+var code_head = 'import math\n'+
+'\n'+
+'def math_map(v, al, ah, bl, bh):\n'+
+'    return bl +  (bh - bl) * (v - al) / (ah - al)\n'+
+'\n'+
+'def math_mean(myList):\n'+
+'    localList = [e for e in myList if type(e) == int or type(e) == float]\n'+
+'    if not localList: return\n'+
+'    return float(sum(localList)) / len(localList)\n'+
+'\n'+
+'def math_median(myList):\n'+
+'    localList = sorted([e for e in myList if type(e) == int or type(e) == float])\n'+
+'    if not localList: return\n'+
+'    if len(localList) % 2 == 0:\n'+
+'        return (localList[len(localList) // 2 - 1] + localList[len(localList) // 2]) / 2.0\n'+
+'    else:\n'+
+'        return localList[(len(localList) - 1) // 2]\n'+
+'\n'+
+'def math_modes(some_list):\n'+
+'    modes = []\n'+
+'    # Using a lists of [item, count] to keep count rather than dict\n'+
+'    # to avoid "unhashable" errors when the counted item is itself a list or dict.\n'+
+'    counts = []\n'+
+'    maxCount = 1\n'+
+'    for item in some_list:\n'+
+'        found = False\n'+
+'        for count in counts:\n'+
+'            if count[0] == item:\n'+
+'                count[1] += 1\n'+
+'                maxCount = max(maxCount, count[1])\n'+
+'                found = True\n'+
+'        if not found:\n'+
+'            counts.append([item, 1])\n'+
+'    for counted_item, item_count in counts:\n'+
+'        if item_count == maxCount:\n'+
+'            modes.append(counted_item)\n'+
+'    return modes\n'+
+'\n'+
+'def math_standard_deviation(numbers):\n'+
+'    n = len(numbers)\n'+
+'    if n == 0: return\n'+
+'    mean = float(sum(numbers)) / n\n'+
+'    variance = sum((x - mean) ** 2 for x in numbers) / n\n'+
+'    return math.sqrt(variance)\n'+
+'\n'+
+'def lists_sort(my_list, type, reverse):\n'+
+'    def try_float(s):\n'+
+'        try:\n'+
+'            return float(s)\n'+
+'        except:\n'+
+'            return 0\n'+
+'    key_funcs = {\n'+
+'        "NUMERIC": try_float,\n'+
+'        "TEXT": str,\n'+
+'        "IGNORE_CASE": lambda s: str(s).lower()\n'+
+'    }\n'+
+'    key_func = key_funcs[type]\n'+
+'    list_cpy = list(my_list)\n'+
+'    return sorted(list_cpy, key=key_func, reverse=reverse)\n'
