c3ef35f3774c3ab41a4202e74293b3da6ed1be4e
xiebochao <xbc1992@yeah.net>
2020-06-21 19:22:49 +0800

    Update mithon.js

--- mixly_arduino/blockly/generators/mithon/mithon.js
+++ mixly_arduino/blockly/generators/mithon/mithon.js
@@ -4,19 +4,19 @@ goog.provide('Blockly.Python.sensor');
 
 goog.require('Blockly.Python');
 
-Blockly.Python['microbit_image_create'] = function(block) {
+Blockly.Python['microbit_image_create'] = function (block) {
     Blockly.Python.definitions_['import_microbit_*'] = 'from microbit import *';
     var colours = {
-      "#000000": "0",
-      "#004400": "1",
-      "#006600": "2",
-      "#008800": "3",
-      "#00aa00": "4",
-      "#00bb00": "5",
-      "#00cc00": "6",
-      "#00dd00": "7",
-      "#00ee00": "8",
-      "#00ff00": "9"
+        "#000000": "0",
+        "#004400": "1",
+        "#006600": "2",
+        "#008800": "3",
+        "#00aa00": "4",
+        "#00bb00": "5",
+        "#00cc00": "6",
+        "#00dd00": "7",
+        "#00ee00": "8",
+        "#00ff00": "9"
     }
     var colour_00 = colours[block.getFieldValue('00')];
     var colour_01 = colours[block.getFieldValue('01')];
@@ -45,19 +45,19 @@ Blockly.Python['microbit_image_create'] = function(block) {
     var colour_44 = colours[block.getFieldValue('44')];
     var code = 'Image("' + colour_00 + colour_01 + colour_02 + colour_03 + colour_04 + ':' + colour_10 + colour_11 + colour_12 + colour_13 + colour_14 + ':' + colour_20 + colour_21 + colour_22 + colour_23 + colour_24 + ':' + colour_30 + colour_31 + colour_32 + colour_33 + colour_34 + ':' + colour_40 + colour_41 + colour_42 + colour_43 + colour_44 + '")';
     return [code, Blockly.Python.ORDER_ATOMIC];
-  };
-  
+};
+
 
 Blockly.Python.base_loop = function (a) {
     //var b = "UNTIL" == a.getFieldValue("MODE"),
     //var c = Blockly.Python.valueToCode(a, "BOOL", b ? Blockly.Python.ORDER_LOGICAL_NOT : Blockly.Python.ORDER_NONE) || "False",
     var d = Blockly.Python.statementToCode(a, "DO"),
-    d = Blockly.Python.addLoopTrap(d, a.id) || Blockly.Python.PASS;
+        d = Blockly.Python.addLoopTrap(d, a.id) || Blockly.Python.PASS;
     Blockly.Python.loops_['base_loop'] = d;
     return "";
 };
 
-Blockly.Python.actuator_rgb_color=function(){
+Blockly.Python.actuator_rgb_color = function () {
     var value_led = this.getFieldValue('LED');
     var values = this.getFieldValue('COLOR').split(",");
     var value_rvalue = values[0];
@@ -66,18 +66,18 @@ Blockly.Python.actuator_rgb_color=function(){
     Blockly.Python.definitions_['import_microbit_*'] = 'from microbit import *';
     Blockly.Python.definitions_['import_neopixel'] = 'import neopixel';
     Blockly.Python.setups_['mixly_rgb_init'] = 'np = neopixel.NeoPixel(pin12, 4)\n';
-    if (value_led>0){
+    if (value_led > 0) {
         Blockly.Python.setups_['mixly_rgb_show'] = Blockly.Python.FUNCTION_MIXLY_RGB_SHOW;
-        var code ='mixly_rgb_show(' + (value_led - 1) + ', ' + value_rvalue + ', ' + value_gvalue + ', ' + value_bvalue + ')\n';
+        var code = 'mixly_rgb_show(' + (value_led - 1) + ', ' + value_rvalue + ', ' + value_gvalue + ', ' + value_bvalue + ')\n';
     }
-    else{
+    else {
         Blockly.Python.setups_['mixly_rgb_show_all'] = Blockly.Python.FUNCTION_MIXLY_RGB_SHOW_ALL;
-        var code ='mixly_rgb_show_all(' + value_rvalue + ', ' + value_gvalue + ', ' + value_bvalue + ')\n';
+        var code = 'mixly_rgb_show_all(' + value_rvalue + ', ' + value_gvalue + ', ' + value_bvalue + ')\n';
     }
     return code;
-  };
+};
 
-Blockly.Python.actuator_rgb_off=function(){
+Blockly.Python.actuator_rgb_off = function () {
     var value_led = this.getFieldValue('LED');
     var value_rvalue = 0;
     var value_gvalue = 0;
@@ -85,17 +85,17 @@ Blockly.Python.actuator_rgb_off=function(){
     Blockly.Python.definitions_['import_microbit_*'] = 'from microbit import *';
     Blockly.Python.definitions_['import_neopixel'] = 'import neopixel';
     Blockly.Python.setups_['mixly_rgb_init'] = 'np = neopixel.NeoPixel(pin12, 4)\n';
-    if (value_led>0){
-        Blockly.Python.setups_['mixly_rgb_show'] = Blockly.Python.FUNCTION_MIXLY_RGB_SHOW;    
-        var code ='mixly_rgb_show(' + (value_led - 1) + ', ' + value_rvalue + ', ' + value_gvalue + ', ' + value_bvalue + ')\n';
-    }else{
+    if (value_led > 0) {
+        Blockly.Python.setups_['mixly_rgb_show'] = Blockly.Python.FUNCTION_MIXLY_RGB_SHOW;
+        var code = 'mixly_rgb_show(' + (value_led - 1) + ', ' + value_rvalue + ', ' + value_gvalue + ', ' + value_bvalue + ')\n';
+    } else {
         Blockly.Python.setups_['mixly_rgb_show_all'] = Blockly.Python.FUNCTION_MIXLY_RGB_SHOW_ALL;
-        var code ='mixly_rgb_show_all(' + value_rvalue + ', ' + value_gvalue + ', ' + value_bvalue + ')\n';
+        var code = 'mixly_rgb_show_all(' + value_rvalue + ', ' + value_gvalue + ', ' + value_bvalue + ')\n';
     }
     return code;
-  };
+};
 
-Blockly.Python.actuator_rgb=function(){
+Blockly.Python.actuator_rgb = function () {
     var value_led = Blockly.Python.valueToCode(this, '_LED_', Blockly.Python.ORDER_ATOMIC);
     var value_rvalue = Blockly.Python.valueToCode(this, 'RVALUE', Blockly.Python.ORDER_ATOMIC);
     var value_gvalue = Blockly.Python.valueToCode(this, 'GVALUE', Blockly.Python.ORDER_ATOMIC);
@@ -104,121 +104,121 @@ Blockly.Python.actuator_rgb=function(){
     Blockly.Python.definitions_['import_neopixel'] = 'import neopixel';
     Blockly.Python.setups_['mixly_rgb_init'] = 'np = neopixel.NeoPixel(pin12, 4)\n';
     Blockly.Python.setups_['mixly_rgb_show'] = Blockly.Python.FUNCTION_MIXLY_RGB_SHOW;
-    var code ='mixly_rgb_show(' + value_led + '-1, ' + value_rvalue + ', ' + value_gvalue + ', ' + value_bvalue + ')\n';
+    var code = 'mixly_rgb_show(' + value_led + '-1, ' + value_rvalue + ', ' + value_gvalue + ', ' + value_bvalue + ')\n';
     return code;
-  };
+};
 
-Blockly.Python.actuator_motor_on = function(){
+Blockly.Python.actuator_motor_on = function () {
     var n = this.getFieldValue('NUMBER');
     var v = Blockly.Python.valueToCode(this, 'SPEED', Blockly.Python.ORDER_ATOMIC);
     var d = this.getFieldValue('DIRECTION');
     Blockly.Python.definitions_['import_microbit_*'] = 'from microbit import *';
-    if(n==0){
+    if (n == 0) {
         Blockly.Python.setups_['mixly_motor1'] = Blockly.Python.FUNCTION_MIXLY_MOTOR1;
-        Blockly.Python.setups_['mixly_motor2'] = Blockly.Python.FUNCTION_MIXLY_MOTOR2; 
+        Blockly.Python.setups_['mixly_motor2'] = Blockly.Python.FUNCTION_MIXLY_MOTOR2;
         Blockly.Python.setups_['mixly_motor3'] = Blockly.Python.FUNCTION_MIXLY_MOTOR3;
-        var code = 'motor1(' + v + ', ' + d +')\nmotor2(' + v + ', ' + d +')\nmotor3(' + v + ', ' + d +')\n';
+        var code = 'motor1(' + v + ', ' + d + ')\nmotor2(' + v + ', ' + d + ')\nmotor3(' + v + ', ' + d + ')\n';
     }
-    else if(n==1){
+    else if (n == 1) {
         Blockly.Python.setups_['mixly_motor1'] = Blockly.Python.FUNCTION_MIXLY_MOTOR1;
-        var code = 'motor1(' + v + ', ' + d +')\n';
+        var code = 'motor1(' + v + ', ' + d + ')\n';
     }
-    else if(n==2){
-        Blockly.Python.setups_['mixly_motor2'] = Blockly.Python.FUNCTION_MIXLY_MOTOR2;        
-        var code = 'motor2(' + v + ', ' + d +')\n';
+    else if (n == 2) {
+        Blockly.Python.setups_['mixly_motor2'] = Blockly.Python.FUNCTION_MIXLY_MOTOR2;
+        var code = 'motor2(' + v + ', ' + d + ')\n';
     }
-    else if(n==3){
+    else if (n == 3) {
         Blockly.Python.setups_['mixly_motor3'] = Blockly.Python.FUNCTION_MIXLY_MOTOR3;
-        var code = 'motor3(' + v + ', ' + d +')\n';
-    }    
+        var code = 'motor3(' + v + ', ' + d + ')\n';
+    }
     return code;
 };
 
-Blockly.Python.actuator_motor_off = function(){
+Blockly.Python.actuator_motor_off = function () {
     var n = this.getFieldValue('NUMBER');
     Blockly.Python.definitions_['import_microbit_*'] = 'from microbit import *';
-    if(n==0){
+    if (n == 0) {
         Blockly.Python.setups_['mixly_motor1'] = Blockly.Python.FUNCTION_MIXLY_MOTOR1;
-        Blockly.Python.setups_['mixly_motor2'] = Blockly.Python.FUNCTION_MIXLY_MOTOR2; 
+        Blockly.Python.setups_['mixly_motor2'] = Blockly.Python.FUNCTION_MIXLY_MOTOR2;
         Blockly.Python.setups_['mixly_motor3'] = Blockly.Python.FUNCTION_MIXLY_MOTOR3;
         var code = 'motor1(0)\nmotor2(0)\nmotor3(0)\n';
     }
-    else if(n==1){
+    else if (n == 1) {
         Blockly.Python.setups_['mixly_motor1'] = Blockly.Python.FUNCTION_MIXLY_MOTOR1;
         var code = 'motor1(0)\n';
     }
-    else if(n==2){
-        Blockly.Python.setups_['mixly_motor2'] = Blockly.Python.FUNCTION_MIXLY_MOTOR2;        
+    else if (n == 2) {
+        Blockly.Python.setups_['mixly_motor2'] = Blockly.Python.FUNCTION_MIXLY_MOTOR2;
         var code = 'motor2(0)\n';
     }
-    else if(n==3){
+    else if (n == 3) {
         Blockly.Python.setups_['mixly_motor3'] = Blockly.Python.FUNCTION_MIXLY_MOTOR3;
         var code = 'motor3(0)\n';
-    }    
+    }
     return code;
 };
 
-Blockly.Python.sensor_pin_near = function(){
+Blockly.Python.sensor_pin_near = function () {
     var number = this.getFieldValue('NUMBER');
-    var code = 'pin'+number+'.read_analog()';
+    var code = 'pin' + number + '.read_analog()';
     return [code, Blockly.Python.ORDER_ATOMIC];
 };
 
 Blockly.Python.sensor_while_btn_pressed = function (a) {
     Blockly.Python.definitions_['import_microbit_*'] = 'from microbit import *';
     var btn = Blockly.Python.valueToCode(this, 'btn', Blockly.Python.ORDER_ATOMIC);
-    Blockly.Python.setups_['on_'+btn] = 'def on_'+btn+'():\n' +
-                                        '    while True:\n' +
-                                        '        if '+btn+'.was_pressed():\n' +
-                                        '            yield callback_'+btn+'()\n' +
-                                        '        else:\n' +
-                                        '            yield\n' +
-                                        '\n' +
-                                        'func_'+btn+' = on_'+btn+'()\n'
+    Blockly.Python.setups_['on_' + btn] = 'def on_' + btn + '():\n' +
+        '    while True:\n' +
+        '        if ' + btn + '.was_pressed():\n' +
+        '            yield callback_' + btn + '()\n' +
+        '        else:\n' +
+        '            yield\n' +
+        '\n' +
+        'func_' + btn + ' = on_' + btn + '()\n'
     var d = Blockly.Python.statementToCode(a, "DO"),
-    d = Blockly.Python.addLoopTrap(d, a.id) || Blockly.Python.PASS;
-    Blockly.Python.loops_[btn+'_loop'] = '    next(func_' + btn + ')\n';
-    return "def callback_"+ btn +"():\n" + d;
+        d = Blockly.Python.addLoopTrap(d, a.id) || Blockly.Python.PASS;
+    Blockly.Python.loops_[btn + '_loop'] = '    next(func_' + btn + ')\n';
+    return "def callback_" + btn + "():\n" + d;
 };
 
-Blockly.Python.sensor_while_is_gesture = function(a){
+Blockly.Python.sensor_while_is_gesture = function (a) {
     Blockly.Python.definitions_['import_microbit_*'] = 'from microbit import *';
     var gesture = this.getFieldValue('gesture');
-    Blockly.Python.setups_['on_'+gesture] = 'def on_gesture_'+gesture+'():\n' +
-                                            '    while True:\n' +
-                                            '        if accelerometer.is_gesture("'+gesture+'"):\n' +
-                                            '            yield callback_gesture_'+gesture+'()\n' +
-                                            '        else:\n' +
-                                            '            yield\n' +
-                                            '\n' +
-                                            'func_gesture_'+gesture+' = on_gesture_'+gesture+'()\n'
+    Blockly.Python.setups_['on_' + gesture] = 'def on_gesture_' + gesture + '():\n' +
+        '    while True:\n' +
+        '        if accelerometer.is_gesture("' + gesture + '"):\n' +
+        '            yield callback_gesture_' + gesture + '()\n' +
+        '        else:\n' +
+        '            yield\n' +
+        '\n' +
+        'func_gesture_' + gesture + ' = on_gesture_' + gesture + '()\n'
     var d = Blockly.Python.statementToCode(a, "DO"),
-    d = Blockly.Python.addLoopTrap(d, a.id) || Blockly.Python.PASS;
-    Blockly.Python.loops_[gesture+'_loop'] = '    next(func_gesture_' + gesture + ')\n';
-    return "def callback_gesture_"+ gesture + '():\n' + d;
+        d = Blockly.Python.addLoopTrap(d, a.id) || Blockly.Python.PASS;
+    Blockly.Python.loops_[gesture + '_loop'] = '    next(func_gesture_' + gesture + ')\n';
+    return "def callback_gesture_" + gesture + '():\n' + d;
 };
 
-Blockly.Python.sensor_while_is_near = function(a){
+Blockly.Python.sensor_while_is_near = function (a) {
     Blockly.Python.definitions_['import_microbit_*'] = 'from microbit import *';
     var near = this.getFieldValue('near');
-    Blockly.Python.setups_['on_'+near] = 'def on_near_'+near+'():\n' +
-                                            '    while True:\n' +
-                                            '        if !pin'+near+'.read_digital():\n' +
-                                            '            yield callback_near_'+near+'()\n' +
-                                            '        else:\n' +
-                                            '            yield\n' +
-                                            '\n' +
-                                            'func_near_'+near+' = on_near_'+near+'()\n'
+    Blockly.Python.setups_['on_' + near] = 'def on_near_' + near + '():\n' +
+        '    while True:\n' +
+        '        if not pin' + near + '.read_digital():\n' +
+        '            yield callback_near_' + near + '()\n' +
+        '        else:\n' +
+        '            yield\n' +
+        '\n' +
+        'func_near_' + near + ' = on_near_' + near + '()\n'
     var d = Blockly.Python.statementToCode(a, "DO"),
-    d = Blockly.Python.addLoopTrap(d, a.id) || Blockly.Python.PASS;
-    Blockly.Python.loops_[near+'_loop'] = '    next(func_near_' + near + ')\n';
-    return "def callback_near_"+ near + '():\n' + d;
+        d = Blockly.Python.addLoopTrap(d, a.id) || Blockly.Python.PASS;
+    Blockly.Python.loops_[near + '_loop'] = '    next(func_near_' + near + ')\n';
+    return "def callback_near_" + near + '():\n' + d;
 };
 
-Blockly.Python.controls_repeat_ext = function(a){
+Blockly.Python.controls_repeat_ext = function (a) {
     Blockly.Python.definitions_['import_microbit_*'] = 'from microbit import *';
     var times = Blockly.Python.valueToCode(this, 'TIMES', Blockly.Python.ORDER_ATOMIC);
     var d = Blockly.Python.statementToCode(a, "DO"),
-    d = Blockly.Python.addLoopTrap(d, a.id) || Blockly.Python.PASS;
+        d = Blockly.Python.addLoopTrap(d, a.id) || Blockly.Python.PASS;
     return 'for _my_variable in range(' + times + '):\n' + d;
 };
